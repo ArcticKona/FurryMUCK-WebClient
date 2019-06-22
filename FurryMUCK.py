a68baf4c087025cd7e10f01ef2176247be3e4b4e
Tuoran Zhang <tigerjieer@163.com>
2019-06-21 21:14:10 -0400

    Add files via upload

---
+++ FurryMUCK.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import ssl
+import asyncio
+import websockets
+print ( 'FurryMUCK TCP-WebSocket Relay Server. 2019 Arctic Kona. No rights reserved. http://arcticjieer.fam.cx/ mailto:arcticjieer@gmail.com' )
+
+host = 'furrymuck.com'
+port = 8899
+localhost = '0.0.0.0'
+localport = '2001'
+sslcontext = ssl.create_default_context ( cafile = 'FurryMUCK.pem' )
+
+async def tomuck ( websocket , socketwrite ):
+	while True:
+		try:
+			msg = await websocket.recv( )
+		except:
+			break
+		socketwrite.write ( msg.encode( 'UTF-8' ) )
+	# close connection when websocket dies
+	socketwrite.close( )
+	await wait_close( )
+
+async def frommuck ( websocket , socketread ):
+	while ( not socketread.at_eof ( ) ):
+		msg = await socketread.read( 4096 )
+		await websocket.send( msg.decode( 'UTF-8' ) )
+	# when TCP dies
+	websocket.send ( 'connection close' )
+
+async def muck ( websocket , path ):
+	socketread , socketwrite = await asyncio.open_connection ( host , port , ssl = sslcontext )
+	# Prepare tasks
+	tomuck_task = asyncio.ensure_future( tomuck ( websocket , socketwrite ) )
+	frommuck_task = asyncio.ensure_future( frommuck ( websocket , socketread ) )
+	await tomuck_task
+	await frommuck_task
+
+asyncio.get_event_loop( ).run_until_complete( websockets.serve ( muck , localhost , localport ) )
+asyncio.get_event_loop( ).run_forever( )
+
+
